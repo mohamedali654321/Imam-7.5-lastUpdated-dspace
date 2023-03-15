e4825ee323af6728880dd6c9d8d7f078b71e0f67
root <root@ip-172-31-13-9.eu-west-1.compute.internal>
2023-03-15 11:59:09 +0300

    first commit

--- src/app/entity-groups/research-entities/item-list-elements/search-result-list-elements/sub-org-unit/sub-org-unit-search-result-list-element.component.ts
+++ src/app/entity-groups/research-entities/item-list-elements/search-result-list-elements/sub-org-unit/sub-org-unit-search-result-list-element.component.ts
@@ -3,7 +3,7 @@ import { ViewMode } from '../../../../../core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
 import { ItemSearchResultListElementComponent } from '../../../../../shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component';
 
-@listableObjectComponent('Administration', ViewMode.ListElement)
+@listableObjectComponent('AdministrationSearchResult', ViewMode.ListElement)
 @Component({
   selector: 'ds-sub-org-unit-search-result-list-element',
   styleUrls: ['./sub-org-unit-search-result-list-element.component.scss'],
